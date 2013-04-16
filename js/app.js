41219a726f6d050eabfb6ff0580fb96d9eeb5b9e
Simon Nguyen <simon@simtechmedia.com>
2013-04-16 21:35:23 +1000

    going to try to implement processing libary

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var FPS = 60;
 
 var canvas;
 var stage;
-var shape;
+
 var circleRadius=30;
 var rings = 30;
 
@@ -11,6 +11,9 @@ var numOfParticles = 100;
 
 var minLife = 2;
 var maxLife = 5;
+
+var minSize = 1;
+var maxSize = 30;
 
 var colors = ["#828b20", "#b0ac31", "#cbc53d", "#fad779", "#f9e4ad", "#faf2db", "#563512", "#9b4a0b", "#d36600", "#fe8a00", "#f9a71f"];
 
@@ -29,8 +32,7 @@ function init() {
     particleContainer = new createjs.Container();
     stage.addChild(particleContainer);
 
-
-    for (var i=0; i<numOfParticles; i++) {
+    for (var i=0; i < numOfParticles; i++) {
         createParticle();
     }
 
@@ -43,7 +45,6 @@ function init() {
     // start the tick and point it at the window so we can do some work before updating the stage:
     createjs.Ticker.addEventListener("tick", tick);
     createjs.Ticker.setFPS(FPS);
-
 }
 
 /**
@@ -52,15 +53,21 @@ function init() {
 function createParticle()
 {
     var shape = new createjs.Shape();
-    shape.graphics.beginFill("#f9a71f").drawCircle(0,0,circleRadius);
+
+    shape.velocity = new PVector
+
+    shape.graphics.setStrokeStyle(2);
+    shape.graphics.beginStroke("#000000");
+    shape.graphics.beginFill("#f9a71f").drawCircle(1,1,circleRadius-2);
     shape.x = canvas.width / 2 ;
     shape.y = canvas.height / 2 ;
     shape.velX = Math.random()*10-5;
     shape.velY = Math.random()*10-5;
+    shape.scaleX = shape.scaleY = randomFromInterval(minSize,maxSize)/20;
     shape.alpha = 1;
     shape.cache(-circleRadius, -circleRadius, circleRadius*2, circleRadius*2);              // Set Cache
     shape.snapToPixel = true;
-    shape.ttl = shape.totallife = randomFromInterval( minLife , maxLife ) * FPS;       //Init shape Time to live
+    shape.ttl = shape.totallife = randomPrecision( minLife , maxLife , 2 ) * FPS;       //Init shape Time to live
     particleContainer.addChild(shape);
 }
 
@@ -74,23 +81,33 @@ function tick()
     var h = canvas.height;
     var l = particleContainer.getNumChildren()-1;
     // iterate through all the children and move them according to their velocity:
-    for (var i=0; i<l; i++) {
+    for (var i=0; i<l-1; i++)
+    {
         var shape = particleContainer.getChildAt(i);
-        if('ttl' in shape )               // Make sure it's a Shape
+        if (typeof(shape.x) !== 'undefined')
         {
             shape.x = (shape.x+shape.velX+w)%w;
             shape.y = (shape.y+shape.velY+h)%h;
             shape.alpha = shape.ttl / shape.totallife;
             shape.ttl--;
             if(shape.ttl < 1 ) {
-                particleContainer.removeChild(shape);
-                shape = null;
+                shape.remove = true;
             }
         }
     }
-    var currentNumberOfShapes = stage.getNumChildren()-1;
-    if( currentNumberOfShapes < numOfParticles )
+    // Do the Remove Afterwards
+    for ( var i = l ; i > 0 ; i--)
     {
+        var shape = particleContainer.getChildAt(i);
+        if(shape.remove === true)
+        {
+            shape.uncache();
+            particleContainer.removeChild(shape);
+        }
+    }
+
+    var currentNumberOfShapes = particleContainer.getNumChildren()-1;
+    if( currentNumberOfShapes < numOfParticles )  {
         var neededNumber = numOfParticles - currentNumberOfShapes;
         for (var i = 0; i < neededNumber ; i++) {
             createParticle();
@@ -107,6 +124,11 @@ function randomFromInterval(from,to)
     return Math.floor(Math.random()*(to-from+1)+from);
 }
 
+function randomPrecision(lo, hi, prec){
+    prec= Math.floor(Math.random()*(prec+1));
+    return Number((lo+ Math.random()*(hi-lo+1)).toFixed(prec));
+}
+
 /**
  * Resize event handler
  */
@@ -119,3 +141,52 @@ function resize() {
     canvas.x = stage.canvas.width / 2;
     canvas.y = stage.canvas.height / 2;
 }
+
+/**
+ * Jquery Stuff to handle the UI
+ */
+$(function() {
+
+    // Amount Slider
+    $( "#slider-amount" ).slider({
+        range: true,
+        min: 0,
+        max: 200,
+        values: [ 50, 100 ],
+        slide: function( event, ui ) {
+            $( "#partAmount" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+        }
+    });
+    $( "#partAmount" ).val( $( "#slider-amount" ).slider( "values", 0 ) +
+        " -" + $( "#slider-amount" ).slider( "values", 1 ) );
+
+    // Size Slider
+    $( "#slider-size" ).slider({
+        range: true,
+        min: 10,
+        max: 50,
+        values: [ minSize, maxSize ],
+        slide: function( event, ui ) {
+            $( "#partSize" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+            minSize = ui.values[ 0 ];
+            maxSize = ui.values[ 1 ];
+            console.log(maxSize)
+        }
+    });
+    $( "#partSize" ).val( $( "#slider-size" ).slider( "values", 0 ) +
+        " -" + $( "#slider-size" ).slider( "values", 1 ) );
+
+    // Speed Slider
+    $( "#slider-speed" ).slider({
+        range: true,
+        min: 10,
+        max: 40,
+        values: [ 15, 20 ],
+        slide: function( event, ui ) {
+            $( "#partSpeed" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+
+        }
+    });
+    $( "#partSpeed" ).val( $( "#slider-speed" ).slider( "values", 0 ) +
+        " -" + $( "#slider-speed" ).slider( "values", 1 ) );
+});
